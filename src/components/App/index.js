2f93ad4bc3d17fb3cf94a8e281e4318f750446b1
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-07-19 22:35:12 -0500

    update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
-import TronWeb from "tronweb";
 
-import Utils from "../../utils";
-import Historial from "../Historial/"
+import Historial from "../Historial/";
 import Oficina from "../Oficina";
 import Oficina2 from "../Oficina2";
 import TronLinkGuide from "../TronLinkGuide";
 import PanelOwner from "../PanelOwner";
-
-
-const FOUNDATION_ADDRESS = "TWiWt5SEDzaEqS6kE5gandWMNfxR2B5xzg";
 
 class App extends Component {
   constructor(props) {
@@ -18,133 +13,435 @@ class App extends Component {
     this.state = {
       tronWeb: {
         installed: false,
-        loggedIn: false
-      }
+        loggedIn: false,
+      },
     };
+
+    this.registrar = this.registrar.bind(this);
+
   }
 
   async componentDidMount() {
-    await new Promise(resolve => {
-      const tronWebState = {
-        installed: !!window.tronWeb,
-        loggedIn: window.tronWeb && window.tronWeb.ready
-      };
 
-      if (tronWebState.installed) {
-        this.setState({
-          tronWeb: tronWebState
-        });
-
-        return resolve();
-      }
-
-      let tries = 0;
-
-      const timer = setInterval(() => {
-        if (tries >= 100) {
-
-          const TRONGRID_API = "https://api.trongrid.io";
-
-          window.tronWeb = new TronWeb(
-            TRONGRID_API,
-            TRONGRID_API,
-            TRONGRID_API
-          );
-
-          this.setState({
-            tronWeb: {
-              installed: false,
-              loggedIn: false
-            }
-          });
-          clearInterval(timer);
-          return resolve();
-        }
-
-        tronWebState.installed = !!window.tronWeb;
-        tronWebState.loggedIn = window.tronWeb && window.tronWeb.ready;
-
-        if (!tronWebState.installed) {
-          return tries++;
-        }
-
-        this.setState({
-          tronWeb: tronWebState
-        });
-
-        resolve();
-      }, 1*1000);
-    });
-
-    if (!this.state.tronWeb.loggedIn) {
-      // Set default address (foundation address) used for contract calls
-      // Directly overwrites the address object if TronLink disabled the
-      // function call
-      window.tronWeb.defaultAddress = {
-        hex: window.tronWeb.address.toHex(FOUNDATION_ADDRESS),
-        base58: FOUNDATION_ADDRESS
-      };
-
-      window.tronWeb.on("addressChange", () => {
-        if (this.state.tronWeb.loggedIn) {
-          return;
-        }
-
-        this.setState({
-          tronWeb: {
-            installed: true,
-            loggedIn: true
-          }
-        });
-      });
-    }
-
-    Utils.setTronWeb(window.tronWeb);
   }
 
-  render() {
-    if (!this.state.tronWeb.installed) return (
-      <>
-        <div className="container">
-        <TronLinkGuide />
-        </div>
-      </>
-      );
+  async registrar(){
 
-    if (!this.state.tronWeb.loggedIn) return (
-      <>
-        <div className="container">
-        <TronLinkGuide installed />
-        </div>
-      </>
+  }
+
+  
+
+  render() {
+      var date = new Date();
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+      var strTime = hours + ':' + minutes + ' ' + ampm;
+
+   
+    
+
+    if (false)
+      return (
+        <>
+          <div className="container">
+            <TronLinkGuide installed />
+          </div>
+        </>
       );
 
     return (
       <>
-      <div className="row contact-content">
+        <div className="wrap">
+          <h2>Tabla Inflables EL SALTO | C.C. Unicentro</h2>
+          <ul className="tabs">
+            <li>
+              <a href="#tab1">
+                <span className="fa fa-home"></span>
+                <span className="tab-text">Inicio</span>
+              </a>
+            </li>
+            <li>
+              <a href="#tab2">
+                <span className="fa fa-group"></span>
+                <span className="tab-text">En Juego</span>
+              </a>
+            </li>
+            <li>
+              <a href="#tab3">
+                <span className="fa fa-briefcase"></span>
+                <span className="tab-text">Afuera</span>
+              </a>
+            </li>
+            <li>
+              <a href="#tab4">
+                <span className="fa fa-bookmark"></span>
+                <span className="tab-text">Historial</span>
+              </a>
+            </li>
+            <li>
+              <a href="#tab1">
+                <span className="fa fa-clock"></span>
+                <span className="tab-text">{strTime}</span>
+              </a>
+            </li>
+          </ul>
 
-        <div className="contact-primary">
+          <div className="secciones">
+            <article id="tab1">
+              <table className="table table-bordered">
+                <caption>
+                  <button
+                    className="btn btn-primary"
+                    onclick="RelojS();"
+                    data-toggle="modal"
+                    data-target="#modalNuevo"
+                  >
+                    Agregar nuevo niño
+                    <span className="glyphicon glyphicon-plus"></span>
+                  </button>
+                </caption>
 
-          <div>
+                <tr className="table table-bordered">
+                  <td>Nombre del niño</td>
+                  <td>Precio voleta</td>
+                  <td>Entrada</td>
+                  <td>Salida</td>
+                  <td>Editar</td>
+                  <td>Eliminar</td>
+                </tr>
 
-            <PanelOwner />
-            <Oficina />
+                <tr id="1" className="table table-bordered">
+                  <td>julian alberto</td>
+                  <td>$10000</td>
+                  <td>5:48 PM</td>
+                  <td>6:00 PM</td>
+                  <td>
+                    <button
+                      className="btn btn-warning glyphicon glyphicon-pencil"
+                      data-toggle="modal"
+                      data-target="#modalEdicion"
+                      onclick="agregaform(' $datos ')"
+                    ></button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger glyphicon glyphicon-remove"
+                      onclick="preguntarSiNo(' $ver[0] ')"
+                    ></button>
+                  </td>
+                </tr>
+
+                <tr id="1">
+                  <td>julian alberto</td>
+                  <td>$10000</td>
+                  <td>5:48 PM</td>
+                  <td>6:00 PM</td>
+                  <td>
+                    <button
+                      className="btn btn-warning glyphicon glyphicon-pencil"
+                      data-toggle="modal"
+                      data-target="#modalEdicion"
+                      onclick="agregaform(' $datos ')"
+                    ></button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger glyphicon glyphicon-remove"
+                      onclick="preguntarSiNo(' $ver[0] ')"
+                    ></button>
+                  </td>
+                </tr>
+              </table>
+            </article>
+            <article id="tab2">
+              <h1>Nosotros</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                voluptates unde, consequuntur aliquid architecto rem numquam
+                expedita minima dolorem pariatur recusandae, eius quod quia
+                aspernatur id impedit, tenetur! Aspernatur incidunt molestiae
+                dolores animi ea praesentium ipsam tenetur voluptas cupiditate
+                perspiciatis eum nihil, natus exercitationem libero earum fuga
+                dignissimos impedit numquam, quasi, placeat officiis voluptates,
+                ad reprehenderit fugiat? Fugiat aperiam et magni, molestiae,
+                numquam consectetur vitae sapiente cupiditate totam laboriosam
+                voluptate obcaecati, aliquam placeat? Suscipit dolores fuga
+                laudantium sed, qui magni iusto dolore quia. Quis fugit
+                exercitationem porro. Rerum nihil omnis recusandae ratione fuga
+                alias eligendi, earum sunt veritatis praesentium eum
+                perspiciatis. Molestias deserunt, iure neque animi quod! Impedit
+                reprehenderit cumque, numquam velit quae cum eius quidem
+                similique laudantium hic deleniti!
+              </p>
+            </article>
+            <article id="tab3">
+              <h1>Servicios</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea
+                numquam odio voluptate. Aliquam incidunt similique, et quasi
+                ducimus quos aut autem non dignissimos dicta sit provident,
+                voluptatibus ut blanditiis perspiciatis cum, vel temporibus
+                minima enim. Asperiores omnis placeat officiis a tenetur sit
+                recusandae, reprehenderit neque. Tempora quibusdam, perferendis
+                id ratione culpa dolorum! Nemo, animi?
+              </p>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatum dignissimos at esse, ipsum rerum assumenda nisi
+                obcaecati! Aliquam iure voluptatem incidunt, explicabo sit
+                labore, perferendis eius ad vel quia. Praesentium, doloribus.
+                Quisquam provident nostrum totam itaque debitis, minima, tempore
+                dolores!
+              </p>
+            </article>
+            <article id="tab4">
+              <h1>Blog</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea
+                numquam odio voluptate. Aliquam incidunt similique, et quasi
+                ducimus quos aut autem non dignissimos dicta sit provident,
+                voluptatibus ut blanditiis perspiciatis cum, vel temporibus
+                minima enim. Asperiores omnis placeat officiis a tenetur sit
+                recusandae, reprehenderit neque. Tempora quibusdam, perferendis
+                id ratione culpa dolorum! Nemo, animi? Eveniet eaque
+                perspiciatis, libero quia, pariatur iusto, ipsum porro quod, ut
+                tempora cum quo non illum. Non eligendi incidunt sequi,
+                molestias quia perspiciatis architecto repudiandae quod.
+              </p>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+                ipsa ducimus amet at cumque sed numquam, explicabo impedit optio
+                quas iste aperiam quidem ipsam rerum libero voluptatibus
+                perferendis officiis voluptatum!
+              </p>
+            </article>
           </div>
         </div>
-        <div className="contact-secondary">
-          <div className="contact-info">
-            <Oficina2 />
-            <Historial />
+
+        <div
+          className="modal fade"
+          id="modalNuevo"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="myModalLabel"
+        >
+          <div className="modal-dialog modal-sm" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 className="modal-title" id="myModalLabel">
+                  Agrega nueva persona
+                </h4>
+              </div>
+              <div className="modal-body">
+                <label>Nombre del niño</label>
+                <input
+                  type="text"
+                  name=""
+                  id="nombre"
+                  className="form-control input-sm"
+                />
+                <label>Responsable</label>
+                <input
+                  type="text"
+                  name=""
+                  id="apellido"
+                  className="form-control input-sm"
+                />
+                <label>Entrada</label>
+                <input
+                  type="text"
+                  id="email"
+                  onclick="RelojS();"
+                  className="form-control input-sm"
+                />
+                <label>salida</label>
+                <input
+                  type="text"
+                  id="telefono"
+                  className="form-control input-sm"
+                />
+                <label>Tiempo</label>
+                <br />
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma3"
+                >
+                  +3 min
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma15"
+                >
+                  +15 min
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma30"
+                >
+                  +30 min
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma60"
+                >
+                  +60 min
+                </button>
+                <p>
+                  <button
+                    type="button"
+                    className="btn btn-warning"
+                    onclick="RelojS();"
+                    id="restablecer"
+                  >
+                    Reiniciar tiempo
+                  </button>
+                </p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-dismiss="modal"
+                  id="guardarnuevo"
+                >
+                  Agregar
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
-      </div>
+        <div
+          className="modal fade"
+          id="modalEdicion"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="myModalLabel"
+        >
+          <div className="modal-dialog modal-sm" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 className="modal-title" id="myModalLabel">
+                  Actualizar datos
+                </h4>
+              </div>
+              <div className="modal-body">
+                <input type="text" hidden="" id="idpersona" name="" />
+                <label>Nombre del niño</label>
+                <input
+                  type="text"
+                  name=""
+                  id="nombreu"
+                  className="form-control input-sm"
+                />
+                <label>Responsable</label>
+                <input
+                  type="text"
+                  name=""
+                  id="apellidou"
+                  className="form-control input-sm"
+                />
+                <label>Entrada</label>
+                <input
+                  type="text"
+                  name=""
+                  id="emailu"
+                  className="form-control input-sm"
+                />
+                <label>Salida</label>
+                <input
+                  type="text"
+                  name=""
+                  id="telefonou"
+                  className="form-control input-sm"
+                />
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma3u"
+                >
+                  +3 min
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma15u"
+                >
+                  +15 min
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma30u"
+                >
+                  +30 min
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  onclick="RelojS();"
+                  id="suma60u"
+                >
+                  +60 min
+                </button>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-warning"
+                  id="actualizadatos"
+                  data-dismiss="modal"
+                >
+                  Actualizar
+                </button>
+
+                <button
+                  type="button"
+                  className="btn btn-danger close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true"> Cancelar &times;</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
-
-
-
-      );
-
+    );
   }
 }
 export default App;
